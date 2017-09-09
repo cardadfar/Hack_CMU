8ab98ae4f14648e3df0845c99f21184c55142213
Michael Becker <mwbecker@cfa323-07.andrew.cmu.edu>
2017-09-09 13:13:18 -0400

    more files

---
+++ BusBuddy/app/src/main/java/com/example/mwbecker/busbuddy/activity10.java
@@ -0,0 +1,21 @@
+package com.example.mwbecker.busbuddy;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.mwbecker.busbuddy.R.id.button;
+
+public class activity10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity10);
+
+    }
+
+}
